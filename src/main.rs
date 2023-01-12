c9e1bd2a764d52a60bf70196ff5764b9e5a3105c
Nate Ferreira <nferreira1729@gmail.com>
2023-01-11 23:00:42 -0500

    refactor: apply rustfmt styling

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,21 @@ use student::Student;
 
 fn validate_group(group: &[Student]) -> bool {
     for pair in group.iter().combinations(2) {
-        if !pair[0].can_work_with(pair[1]) { return false; }
-        if !pair[1].can_work_with(pair[0]) { return false; }
+        if !pair[0].can_work_with(pair[1]) {
+            return false;
+        }
+        if !pair[1].can_work_with(pair[0]) {
+            return false;
+        }
     }
-    true    
+    true
 }
 
 fn validate_student_list(students: &Vec<Student>, group_size: usize) -> bool {
     let n_groups = students.len() / group_size;
 
     for offset in 0..n_groups {
-        let group = &students[offset*group_size..(offset+1)*group_size];
+        let group = &students[offset * group_size..(offset + 1) * group_size];
         if !validate_group(group) {
             return false;
         }
@@ -36,14 +40,16 @@ fn main() {
             let n_groups = student_cnt / 4;
 
             for offset in 0..n_groups {
-                println!("=== Group {} ===", offset+1);
-                println!("{}\n{}\n{}\n{}",
-                         arrangement[offset*4].name,
-                         arrangement[offset*4+1].name,
-                         arrangement[offset*4+2].name,
-                         arrangement[offset*4+3].name)
+                println!("=== Group {} ===", offset + 1);
+                println!(
+                    "{}\n{}\n{}\n{}",
+                    arrangement[offset * 4].name,
+                    arrangement[offset * 4 + 1].name,
+                    arrangement[offset * 4 + 2].name,
+                    arrangement[offset * 4 + 3].name
+                )
             }
             break;
-        } 
+        }
     }
 }
